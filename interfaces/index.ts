6ad869abb918479fb7f272b5c0be5437019fe5a8
celinedotcom <celineniyinkunda@gmail.com>
2025-07-20 22:57:38 +0200

    Implement Posts Card Components with PostCard and PostProps interface

---
+++ interfaces/index.ts
@@ -0,0 +1,12 @@
+export interface PostProps {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+
+  export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
