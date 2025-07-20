1ca123ed3420741096c26139ba10a908a2144993
celinedotcom <celineniyinkunda@gmail.com>
2025-07-20 22:17:49 +0200

    Update Header and add posts/users pages

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,9 +1,14 @@
-const Home: React.FC = () => {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-7xl font-thin">Welcome Page</h1>
-    </div>
-  )
-}
+import Header from "@/components/layout/Header";
 
- export default Home;
+const Posts: React.FC = () => {
+  return (
+    <div className="h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow flex items-center justify-center bg-blue-100">
+        <h2 className="text-3xl font-bold">Posts Page</h2>
+      </main>
+    </div>
+  );
+};
+
+export default Posts;
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,9 +1,14 @@
-const Home: React.FC = () => {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-7xl font-thin">Welcome Page</h1>
-    </div>
-  )
-}
+import Header from "@/components/layout/Header";
 
- export default Home;
+const Users: React.FC = () => {
+  return (
+    <div className="h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow flex items-center justify-center bg-green-100">
+        <h2 className="text-3xl font-bold">Users Page</h2>
+      </main>
+    </div>
+  );
+};
+
+export default Users;
